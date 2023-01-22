5159d6843b1b3bc975ef0d840ffb9b2138c651d9
Gar1b1 <alongaribi123@gmail.com>
2023-01-22 15:06:55 +0200

    continue to build group class.
    Signed-off-by: Gar1b1 <alongaribi123@gmail.com>

--- group.py
+++ group.py
@@ -1,12 +1,21 @@
+import pytz
+from datetime import datetime
+from firebase_admin import firestore, credentials, storage
+
 class Group():
-    def __init__(self, owner, isGhost, dbLocation):
+    def __init__(self,db, owner, isGhost, dbID, fb):
         self.owner = owner
         self.isGhost = isGhost
         self.participants = [owner]
         self.TextChatRooms = []
         self.VoiceChatRooms = []
-        self.db_location = dbLocation
+        self.dbID = dbID
+        self.db = db
+        self.dbLocation = self.db.collection('servers').where("ID", '==', ID)
         self.admins = [owner]
+        self.timestamp_loc = pytz.timezone('Asia/Jerusalem')
+        self.storage = fb.storage()
+
 
     def add_participants(self, adder, participant):
         if participant in self.participants:
@@ -44,4 +53,22 @@ class Group():
         return "don't have permission"
 
     def add_text_message(self, participant, message):
-        self.db_location
+        if participant in self.participants:
+            messageObject = {'user' : participant, 'data': message, 'time': datetime.now(self.timestamp_loc), 'type' : 'text'}
+            self.dbLocation.update({u'messages': firstore.ArrayUnion([messageObject])})
+        else:
+            return "not in group"
+
+    def add_image_message(self, participant, image):
+        isExists = False
+        if participant in self.participants:
+            while not isExists:
+                self.storage.child("Chats/")
+                letters = string.ascii_letters + string.digits
+                imgID = ''.join(random.choice(letters) for i in range(length))
+                img = self.storage.child(f"Chats/{imgID}")
+                isExists = img.exists()[0]
+            messageObject = {'user' : participant, 'data': imgID, 'time': datetime.now(self.timestamp_loc), 'type' : 'image'}
+            self.dbLocation.update({u'messages': firstore.ArrayUnion([messageObject])})
+        else:
+            return "not in group"
